2200a7cf0f7ffeac1c80b4c3a44c01d2f308cdef
scrawach <scrawach@gmail.com>
2024-04-16 00:50:15 +0300

    implemented switching weapons

---
+++ Assets/Weapons/Scripts/Gameplay/SwitchingWeapon.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Weapons.Gameplay.Bullets;
+using Weapons.Gameplay.Weapons;
+
+namespace Weapons.Gameplay
+{
+    public class SwitchingWeapon : IWeapon
+    {
+        private readonly Dictionary<Type, IWeapon> _weapons;
+        private IWeapon _current;
+
+        public SwitchingWeapon(Transform shootPoint, BulletAsset bullet)
+        {
+            _weapons = new Dictionary<Type, IWeapon>
+            {
+                [typeof(Rifle)] = new Rifle(shootPoint, bullet),
+                [typeof(Shotgun)] = new Shotgun(shootPoint, bullet),
+                [typeof(EndlessAmmoWeapon)] = new EndlessAmmoWeapon(shootPoint, bullet)
+            };
+        }
+
+        public void SwitchTo<TWeapon>() where TWeapon : IWeapon
+        {
+            var weaponType = typeof(TWeapon);
+
+            if (_weapons.TryGetValue(weaponType, out var weapon))
+                _current = weapon;
+            else
+                throw new ArgumentOutOfRangeException($"{nameof(SwitchTo)}: {weaponType} not exist!");
+        }
+        
+        public bool HasAmmo() => 
+            _current.HasAmmo();
+
+        public void AddAmmo(int amountOfAmmo) => 
+            _current.AddAmmo(amountOfAmmo);
+
+        public void Shoot() => 
+            _current.Shoot();
+    }
+}
